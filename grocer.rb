5b0f9be10be64fe8a2f880c8bca115f9ad4fdf36
yong liang <yongliang24@gmail.com>
2019-01-23 07:33:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,16 +1,15 @@
 def consolidate_cart(cart)
   # adds a count of one to each item when there are no duplicates
   hash_cart = {}
-
-   cart.each do |foodHash|
-    foodHash.each do |food, foodData|
-      data_cart = hash_cart[food]
+   cart.each do |info|
+    info.each do |key, value|
+      data_cart = hash_cart[key]
       if data_cart == nil
         data_cart = {}
-        hash_cart[food] = foodData
-        hash_cart[food][:count] = 1
+        hash_cart[key] = value
+        hash_cart[key][:count] = 1
       else
-        hash_cart[food][:count] += 1
+        hash_cart[key][:count] += 1
       end
     end
   end
