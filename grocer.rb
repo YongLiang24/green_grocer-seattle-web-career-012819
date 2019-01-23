41cd06dee8916fde5e8bdff69a123fa114973607
yong liang <yongliang24@gmail.com>
2019-01-23 07:44:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,6 +18,19 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupItem = coupon[:item]
+  coupons.each do |i|
+    if cart.keys.include?(coupItem) && cart[coupItem][:count] >= i[:num]
+        cart[coupItem][:count] = cart[coupItem][:count] - i[:num]
+        cart["#{coupItem} W/COUPON"] ||= {}
+        cart["#{coupItem} W/COUPON"][:price] = i[:cost]
+        cart["#{coupItem} W/COUPON"][:clearance] = cart[coupItem][:clearance]
+        cart["#{coupItem} W/COUPON"][:count] ||= 0
+        cart["#{coupItem} W/COUPON"][:count] += 1
+      end
+    
+  end
+    return cart
 end
 
 def apply_clearance(cart)
